3e0ee81c4310c7e67c311b58b242d558a03c9b5e
NataKost <capotny@rambler.ru>
2022-08-27 19:56:06 +0300

    Generation works!

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -5,27 +5,31 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
 
-    private int scores = 0;
+    public int scores = 0;
+    public int prevScores = 0;
     public Transform player;
+    public List<GameObject> ballPref;
     public List<GameObject> snakeBalls;
 
     private void OnTriggerEnter(Collider other)
     {
-        int temp = scores;
+        prevScores = scores;
         scores++;
 
-        for (int i = 0; i < (scores - temp); i++)
+        for (int i = prevScores ; i < scores ; i++)
         {
-            GameObject snakeBall = Instantiate(snakeBalls[0], transform);
+            GameObject snakeBall = Instantiate(ballPref[0], transform);
             snakeBalls.Add(snakeBall);
-
+            
             if (i > 0)
             {
-                snakeBall.transform.localPosition = new Vector3(snakeBalls[i - 1].transform.position.x, snakeBalls[i - 1].transform.position.y, snakeBalls[i - 1].transform.position.z - 0.5f);
+                snakeBall.transform.localScale = new Vector3(1, 1, 1);
+                snakeBall.transform.localPosition = new Vector3(snakeBalls[i - 1].transform.localPosition.x, snakeBalls[i - 1].transform.localPosition.y, snakeBalls[i - 1].transform.localPosition.z - 1f);
             }
             else
             {
-                snakeBall.transform.localPosition = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 0.5f);
+                snakeBall.transform.localScale = new Vector3(1, 1, 1);
+                snakeBall.transform.localPosition = new Vector3(0, 0,-1f);
             }
         }
     }

f3019486ed9389e81aaf58702291796a1defeb3a
NataKost <capotny@rambler.ru>
2022-08-29 20:36:16 +0300

    Rewise generation

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
-    public float speedGround = 1.0f;
+    public float speedGroundMoving = 15.0f;
     public float speedplayer = 5.0f;
-    public Transform ground;
     public Transform player;
 
     // Update is called once per frame
     void Update()
     {
-        ground.transform.position = ground.transform.position - new Vector3 (0, 0, speedGround * Time.deltaTime);
-        
+        player.transform.position = player.transform.position + new Vector3 (0, 0, speedGroundMoving * Time.deltaTime);
+
         if (Input.GetKey (KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             player.transform.position = player.transform.position - new Vector3 (speedplayer * Time.deltaTime, 0, 0);
